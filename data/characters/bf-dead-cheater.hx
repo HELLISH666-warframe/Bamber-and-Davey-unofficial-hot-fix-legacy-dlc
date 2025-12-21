6f19863c64ba6a06e5bcf4718750be5869d6a4ef
HELLISH666-warframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2025-12-21 13:02:15 +1100

    Why_was_this_in_gitignore_bruh.

---
+++ data/characters/bf-dead-cheater.hx
@@ -0,0 +1,5 @@
+//Can't_be_bothered_to_do_a_better_fix.
+function update(elapsed:Float) {
+    this.x=2100;
+    this.y=900;
+}
---
+++ data/characters/cubefreind.hx
@@ -0,0 +1,6 @@
+/*GameOverSubstate.script = 'data/scripts/gameovers/meta';
+function onGameOver(e) {
+    GameOverSubstate.script = 'data/scripts/gameovers/friendly';
+    e.gameOverSong = "death/default";
+}
+*/
